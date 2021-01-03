1a96d34077a3eb3f8b5fc2c61faa635d040d662b
suiranpiza <thomas.hennoe@hotmail.com>
2021-01-03 15:03:24 +0100

    remove schedule event

--- bot.py
+++ bot.py
@@ -36,17 +36,8 @@ async def on_ready():
 @client.event
 async def on_message(message):
     if client.user.id != message.author.id:
-        if "thomas schedule" == message.content.lower():            
-            embedVar = discord.Embed(title="Thomas' Schedule", description="For the coming week", color=0xff0000)
-            embedVar.add_field(name="Monday", value="Busy")
-            embedVar.add_field(name="Tuesday", value="Very Busy")
-            embedVar.add_field(name="Wednesday", value="__**Omega Busy**__")
-            embedVar.add_field(name="Thursday", value="Chilling and GAMING MINECRAFT BABY WOOOOOOOOOOOOOO")
-            embedVar.add_field(name="Friday", value="Doing laundry and PLAYING MINECRAFT WOOOOOO")
-            embedVar.add_field(name="Saturday", value="Going home for smissmas!!")
-            await message.channel.send(embed=embedVar)
 
-        elif 'thomas' in message.content.lower():
+        if 'thomas' in message.content.lower():
             await message.channel.send(f'{message.author.mention} Thomas is currently in a __*meeting*__. Can I take a message?')
             sleep(3)
             await message.channel.send("SIKE! I won't tell him anything. He doesn't have time for your little boy problems")
@@ -56,9 +47,6 @@ async def on_message(message):
 
         if 'haha' in message.content.lower():
             await message.add_reaction(reactionEmoji)
-
-        # if "s" == message.content:
-        #     await client.logout()
 
         await client.process_commands(message)
 
