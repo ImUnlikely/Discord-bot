a5d2e753d720bf42139bfb0f007ad8ce01b8b449
suiranpiza <thomas.hennoe@hotmail.com>
2021-01-04 22:16:26 +0100

    more testing

--- dev.py
+++ dev.py
@@ -1,10 +1,51 @@
+from logging import error
+import os
+import discord
+from discord import utils
+from discord.ext.commands import Bot
+from discord.ext.commands.core import is_owner
+from dotenv import load_dotenv
+from time import sleep
+import pyautogui
 from PIL import Image
-import win32gui, win32ui, win32api, win32con
+import pyautogui
+import win32api
+from win32api import GetSystemMetrics
+import win32con
+import win32gui
+import win32ui
 
-# Get entire virtual screen
+
+toplist, winlist = [], []
+def enum_cb(hwnd, results):
+    winlist.append((hwnd, win32gui.GetWindowText(hwnd)))
+win32gui.EnumWindows(enum_cb, toplist)
+
+server_window = [(hwnd, title) for hwnd, title in winlist if 'system32\cmd.exe' in title.lower()]
+if len(server_window) == 0:
+    print("Window not found")
+    exit()
+
+# Set window as foreground before screenshot
+hwnd = server_window[0][0]
+print(hwnd)
+win32gui.SetForegroundWindow(hwnd)
+
+# Get virtual screen
 hwnd = win32gui.GetDesktopWindow()
+hwndDC = win32gui.GetWindowDC(hwnd)
+mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
+saveDC = mfcDC.CreateCompatibleDC()
 
-# get complete virtual screen including all monitors
+# Get coords and calculate width and height
+# startX, startY, endX, endY = bbox = win32gui.GetWindowRect(hwnd)
+# l = startX
+# t = startY
+# w = startX - endX
+# h = endY - startY
+# w = w*-1 if w<0 else w  # Convert to positive if negative
+# h = h*-1 if h<0 else h  # Convert to positive if negative
+
 SM_XVIRTUALSCREEN = 76
 SM_YVIRTUALSCREEN = 77
 SM_CXVIRTUALSCREEN = 78
@@ -13,14 +54,6 @@ w = vscreenwidth = win32api.GetSystemMetrics(SM_CXVIRTUALSCREEN)
 h = vscreenheigth = win32api.GetSystemMetrics(SM_CYVIRTUALSCREEN)
 l = vscreenx = win32api.GetSystemMetrics(SM_XVIRTUALSCREEN)
 t = vscreeny = win32api.GetSystemMetrics(SM_YVIRTUALSCREEN)
-r = l + w
-b = t + h
-
-print(w, h, l, t, r, b)
-
-hwndDC = win32gui.GetWindowDC(hwnd)
-mfcDC  = win32ui.CreateDCFromHandle(hwndDC)
-saveDC = mfcDC.CreateCompatibleDC()
 
 # Create bitmap
 saveBitMap = win32ui.CreateBitmap()
@@ -30,7 +63,6 @@ saveDC.BitBlt((0, 0), (w, h),  mfcDC,  (l, t),  win32con.SRCCOPY)
 
 # Save bitmap
 bmpinfo = saveBitMap.GetInfo()
-print(bmpinfo)
 bmpstr = saveBitMap.GetBitmapBits(True)
 
 # Save image
